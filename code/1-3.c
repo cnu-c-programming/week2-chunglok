c97b6031256fbb1a99997197d72382a02ed577f8
최준화 <choijunhwa072@gmail.com>
2026-03-18 14:01:35 +0900

    Update 1-3.c

--- code/1-3.c
+++ code/1-3.c
@@ -3,11 +3,11 @@
 int main()
 {
      int age;
-    char grade;
-    float gpa;
+     char grade;
+     float gpa;
 
-    scanf("%d %c %f", &age, &grade, &gpa);
-    printf("%d %1c %1.2f\n", age ,grade , gpa);
+     scanf("%d %c %f\n", &age, &grade, &gpa);
+     printf("%d %1c %1.2f\n", age ,grade , gpa);
 
-    return 0;
+     return 0;
 }
