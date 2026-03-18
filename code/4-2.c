ec9bc36fcead40741932c9241593290e35f30037
최준화 <choijunhwa072@gmail.com>
2026-03-18 13:40:03 +0900

    Update 4-2.c

--- code/4-2.c
+++ code/4-2.c
@@ -2,7 +2,15 @@
 
 int main()
 {
+    int a, i, j;
 
+    for(i = 1; i < 10; i++) {
+        for(j = 1; j < 10; j++) {
+            a = i * j;
+            printf("%d * %d = %d\n", i, j, a);
+        }
+    }
+    
     return 0;
 }
 
