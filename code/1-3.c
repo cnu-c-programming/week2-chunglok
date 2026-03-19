2ad62abb6a83387d47a47adaec0c32f0f0da89a4
최준화 <choijunhwa072@gmail.com>
2026-03-19 10:28:09 +0900

    Fix format specifiers in scanf and printf

--- code/1-3.c
+++ code/1-3.c
@@ -6,8 +6,8 @@ int main()
     char grade;
     float gpa;
 
-    scanf("%d %c %f\n", &age, &grade, &gpa);
-    printf("%d %1c %1.2f\n", age ,grade , gpa);
+    scanf("%d %c %f", &age, &grade, &gpa);
+    printf("%d %c %.2f\n", age ,grade , gpa);
 
     return 0;
 }
